bfe4ff7a10bb5b9b397e7b4b8e131fe62a2f58d6
Parviz Shirinov <parveeezy@gmail.com>
2023-03-05 14:04:22 +0300

    refactoring code & added redux-thunk

--- src/reduxToolkit/toolkitUsersReducer.js
+++ src/reduxToolkit/toolkitUsersReducer.js
@@ -1,5 +1,6 @@
 import { v4 } from 'uuid';
 import { createAction, createReducer } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 
 const initialState = {
@@ -15,6 +16,17 @@ export const decrement = createAction('DECREMENT');
 export const reset = createAction('RESET');
 export const addUser = createAction('ADD_USER');
 export const getUser = createAction('GET_USER');
+export const addManyUser = createAction('ADD_MANY_USERS');
+
+export const fetchUsers = () => {
+
+    return dispatch => {
+        axios('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json =>dispatch(addManyUser(json)));
+
+    };
+};
 
 export const toolkitCounterReducer = createReducer(initialState, {
     [increment]: (state, action) => {
@@ -28,10 +40,14 @@ export const toolkitCounterReducer = createReducer(initialState, {
         state.count = state.count = 0;
     },
     [addUser]: (state, action) => {
-        return {...state, users: [...state.users, {id: v4(), name: action.payload}]}
+        return { ...state, users: [...state.users, { id: v4(), name: action.payload }] };
     },
     [getUser]: (state, action) => {
-        console.log(action);
-        return {...state, users: state.users.filter(u => u.id !== action.payload)}
+        return { ...state, users: state.users.filter(u => u.id !== action.payload) };
+    },
+    [addManyUser]: (state, action) => {
+        return {
+            ...state, users: [...state.users, ...action.payload],
+        };
     },
 });
